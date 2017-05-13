e2c74eb3b7b21c5e012841d25cb03eddebb80c07
santiagobassani96 <santiago.bassani96@gmail.com>
2017-05-13 02:31:16 -0300

    first commit

---
+++ Persona/Person.java
@@ -0,0 +1,44 @@
+public abstract Person {
+	private String dni;
+	private String name;
+	private String surname;
+
+	protected Person(String dni, String name, String surname){
+		if(checkDni(dni) && checkName(name) && checkSurname(surname)){
+			this.dni = dni;
+			this.name = name;
+			this.surname = surname;
+
+		}
+
+	}
+
+	private boolean checkDni(String dni){
+		if(dni != nul && dni.matches("[-+]?\\d*\\.?\\d+")){
+			return true;
+		}
+		return false;
+	}
+
+
+	public boolean equals(Object obj){
+		if(obj == null || this.getClass() != obj.getClass()){
+			return false;
+		}
+
+		Person aux = (Person) obj;
+		return this.getDni() == aux.getDni();
+	}
+
+
+	public String getDni(){
+		return getDni;
+	}
+	public String getName(){
+		return name;
+	}
+
+	public String getSurname(){
+		return surname;
+	}
+}
